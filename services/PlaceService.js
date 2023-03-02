1c07eb2f52dd6c35956bc534210ec5d2a36903c8
shabin007 <shabinshaw@gmail.com>
2023-03-02 10:41:16 +0530

    update

---
+++ services/PlaceService.js
@@ -0,0 +1,10 @@
+/** @format */
+
+// import Placesmodel from "../models/places.js";
+
+// export async function addPlace(placedata) {
+//   console.log(placedata);
+//   const place = new Placesmodel(placedata);
+//   await place.save();
+//   return { place };
+// }
